11248782a34486117d8cd3786ca4625d77f30259
tprince0521 <tprincipe@brooksschool.org>
2026-02-10 14:47:24 +0000

    Deck Created

--- PiratePairs.java
+++ PiratePairs.java
@@ -1,7 +1,19 @@
-public class PiratePairs {
-    public static void main (String[] args) {
+import java.util.Arrays;
 
-        System.out.println("hello");
+public class PiratePairs {
+      public static void main (String[] args) {
+
+        int[] deck = new int[55];
+        int val = 0;
+
+        for(int i = 1; i <= 10; i++){
+            for(int j = 0; j < i; j++){
+                deck[val] = i;
+                val ++;
+            }
+        }
+
+
 
     }
 }
