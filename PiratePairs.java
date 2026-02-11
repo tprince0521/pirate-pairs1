2290da6958a35a2ed3152d0d99d167da2e2ffb4f
tprince0521 <tprincipe@brooksschool.org>
2026-02-11 04:43:21 +0000

    Made shuffler but the method definition isnt working. Will work on tomorrow.

--- PiratePairs.java
+++ PiratePairs.java
@@ -1,4 +1,4 @@
-import java.util.Arrays;
+import java.util.Random;
 
 public class PiratePairs {
       public static void main (String[] args) {
@@ -9,11 +9,32 @@ public class PiratePairs {
         for(int i = 1; i <= 10; i++){
             for(int j = 0; j < i; j++){
                 deck[val] = i;
-                val ++;
+                val++;
             }
         }
 
+        public static Array shuffler(int[] deck) {
 
+             Random rand = new Random();    
+
+            for (int card : deck) {
+                System.out.print(card + " ");
+            } 
+
+            for (int i = deck.length - 1; i > 0; i--) {
+                int j = rand.nextInt(i + 1);
+
+            int temp = deck[i];
+            deck[i] = deck[j];
+            deck[j] = temp;
+            }
+
+        }
+
+        System.out.print("Shuffled: ");
+        for (int card : deck) {
+            System.out.print(card + " ");
+        }
 
     }
 }
