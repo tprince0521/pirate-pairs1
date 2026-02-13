d8f67ef44fc0c5d85089161e61ae315f9d7fbcce
tprince0521 <tprincipe@brooksschool.org>
2026-02-13 02:16:43 +0000

    Shuffle is working, also got show cards and draw cards.

---
+++ Deck.java
@@ -0,0 +1,48 @@
+import java.util.Arrays;
+
+public class Deck {
+
+    int[] cards = new int[55];
+
+    public Deck() {
+
+        int val = 0;
+
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 0; j < i; j++) {
+                cards[val] = i;
+                val++;
+            }
+        }
+    }
+
+    public void shuffle() {
+
+        for (int i = cards.length - 1; i >= 0; i--) {
+            int j = (int) (Math.random() * (i + 1));
+
+            int temp = cards[i];
+            cards[i] = cards[j];
+            cards[j] = temp;
+        }
+    }
+
+    public void show() {
+        System.out.println(Arrays.toString(cards));
+    }
+
+    public int drawCard() {
+
+        int nextCard = cards[cards.length - 1];
+        int[] newDeck = new int[cards.length - 1];
+
+        for (int i = 0; i < cards.length - 1; i++) {
+            newDeck[i] = cards[i];
+        }
+
+        cards = newDeck;
+
+        return nextCard;
+    }
+
+}
--- PiratePairs.java
+++ PiratePairs.java
@@ -1,40 +1,14 @@
-import java.util.Random;
-
 public class PiratePairs {
-      public static void main (String[] args) {
+    public static void main(String[] args) {
+        Deck myDeck = new Deck();
+        myDeck.show();
 
-        int[] deck = new int[55];
-        int val = 0;
+        myDeck.shuffle();
+        myDeck.show();
 
-        for(int i = 1; i <= 10; i++){
-            for(int j = 0; j < i; j++){
-                deck[val] = i;
-                val++;
-            }
-        }
-
-        public static Array shuffler(int[] deck) {
-
-             Random rand = new Random();    
-
-            for (int card : deck) {
-                System.out.print(card + " ");
-            } 
-
-            for (int i = deck.length - 1; i > 0; i--) {
-                int j = rand.nextInt(i + 1);
-
-            int temp = deck[i];
-            deck[i] = deck[j];
-            deck[j] = temp;
-            }
-
-        }
-
-        System.out.print("Shuffled: ");
-        for (int card : deck) {
-            System.out.print(card + " ");
-        }
+        myDeck.drawCard();
+        myDeck.show();
 
     }
+
 }
